1adbfc473313c3a4ef734548c4cbe37794bc2fff
Piotr <petegi.sq@gmail.com>
2018-04-10 11:00:51 +0200

    Changes Scss variables now match bootstrap4 breakpoints, added ScrollTo js class

---
+++ src/scripts/jquery/scrollTo.js
@@ -0,0 +1,18 @@
+import 'jquery';
+class ScrollTo {
+    constructor(selector) {
+        this.selector = selector;
+        this.speed = 500;
+    }
+    init() {
+        $(this.selector).on('click', function(e) {
+            e.preventDefault();
+            let href = $(this).attr('href') ? $(this).attr('href') : $(this).attr('data-href');
+            $('html, body').animate({
+                scrollTop: $(href).offset().top
+            }, this.speed);
+        });
+    }
+}
+
+export default ScrollTo;
